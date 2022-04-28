b3d3c9bb0a92ef8ef1b89176bf4042ad72a8d5a9
justinteng1999 <just.teng@mail.utoronto.ca>
2022-04-27 23:47:29 -0400

    Final Edit

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -5,13 +5,13 @@
 # Contact: just.teng@mail.utoronto.ca 
 
 #### Workspace setup ####
-# Use R Projects, not setwd().
 library(haven)
 library(tidyverse)
+
 # Read in the raw data. 
 raw_data <- read_csv("BMI_raw_data.csv")
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
+
+# Cleaning the data set and selecting the variables of our interest 
 
 reduced_data <- 
   raw_data %>% 
@@ -22,8 +22,51 @@ reduced_data <-
          `Measured adult body mass index`,
          Characteristics,
          VALUE)
+reduced_data <- na.omit(reduced_data)
 
-#rm(raw_data)
+# Cleaning the characteristics and leaving only the number of persons
+# which we need for our analysis
+
+reduced_data <-
+  reduced_data %>%
+  subset(Characteristics != "High 95% confidence interval, number of persons" &
+           Characteristics != "Low 95% confidence interval, number of persons" &
+           Characteristics != "Coefficient of variation for number of persons" &
+           Characteristics != "Percent" &
+           Characteristics != "Low 95% confidence interval, percent" &
+           Characteristics != "High 95% confidence interval, percent" &
+           Characteristics != "Coefficient of variation for percent")
+
+# Combine the the column "VARIABLE" and "Number of Persons" into one column
+
+BMI_cleaned <-
+  reduced_data %>%
+  rename(Number_of_Persons = VALUE) %>%
+  subset(select = -Characteristics)
+
+# Change the variables' names for easier future reference
+BMI_cleaned <-
+  BMI_cleaned %>%
+  rename(BMI =`Measured adult body mass index`)
+
+# Shorten BMI's categories' name
+BMI_cleaned <-
+  BMI_cleaned %>%
+  mutate(BMI = case_when(
+    BMI == "Total population for the variable measured adult body mass index" ~ "Total Population",
+    BMI == "Underweight, measured adult body mass index under 18.50" ~ "Underweight",
+    BMI == "Normal weight, measured adult body mass index 18.50 to 24.99" ~ "Normal Weight",
+    BMI == "Overweight, measured adult body mass index 25.00 to 29.99" ~ "Overweight",
+    BMI == "Obese, measured adult body mass index 30.00 or higher" ~ "Obese",
+    BMI == "Obese class I, measured adult body mass index 30.00 to 34.99" ~ "Obese Class I",
+    BMI == "Obese class II, measured adult body mass index 35.00 to 39.99" ~ "Obese Class II",
+    BMI == "Obese class III, measured adult body mass index 40.00 or higher" ~ "Obese Class III"
+  ))
+
+# Save the cleaned data set into a separate csv
+
+write_csv(BMI_cleaned, "outputs/paper/BMI_cleaned.csv")
+
          
 
 
